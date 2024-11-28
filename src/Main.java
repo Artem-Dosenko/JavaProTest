2cba98c2462aaf04efe4c6f3f0bd14560844e33e
artem <artem.dos.25@gmail.com>
2024-11-28 19:30:24 +0200

    added printer to main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        printer("Artem");
+        printer("Artem ");
     }
     public static void printer(String message){
         System.out.println("Hello, " + message);
