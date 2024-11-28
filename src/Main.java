fd4ad12ea2f15f213b0933a7e5f4d4a4ddff9151
artem <artem.dos.25@gmail.com>
2024-11-28 19:27:57 +0200

    printer added commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        System.out.println("It's me!");
-        System.out.println("Artem");
+    }
+    public static void printer(String message){
+        System.out.println("Hello, " + message);
     }
 }
